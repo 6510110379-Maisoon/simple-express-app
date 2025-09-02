1888af5b71e48841adcfe4bc89a5f7b7567e7b0c
มัยซูน เก็นตาสา <ktsmaisoon@gmail.com>
2025-09-02 11:40:40 +0700

    fix app

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 // app.js
 function add(a, b) {
-    var sum = a + b;  // ใช้ var แทน const → SonarQube จะเตือน
-    console.log("Adding numbers"); // console.log เกินจำเป็น
-    return sum;
+    return a + b;
 }
 
 console.log("2 + 3 =", add(2, 3));
-
-// เพิ่มตัวแปร unused → code smell
-var unusedVar = 123;
